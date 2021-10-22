19d7c3c2d64291a85972b10d959454c446e0eca0
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-10-22 13:52:28 +0530

    Update Factorial.c

--- C/C programs for practise/functions/Factorial.c
+++ C/C programs for practise/functions/Factorial.c
@@ -1,10 +1,17 @@
-int main()
+#include <stdio.h>
+int factorial()
 {   int num;
     scanf("%d",&num);
     int ans=1;
     for(int i=1;i<=num;i++){
         ans*=i;
     }
-    printf("%d",ans);
+    
+    return ans;
+}
+int main(){
+    int a;
+    a=factorial();
+    printf("%d",a);
     return 0;
 }
