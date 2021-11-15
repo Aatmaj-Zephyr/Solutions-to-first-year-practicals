2086f47ddaa09fb631566435c23fd8b4c2349428
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-15 15:45:08 +0530

    Update Pascal's triangle.c

--- C/C programs for practise/Strings/Pascal's triangle.c
+++ C/C programs for practise/Strings/Pascal's triangle.c
@@ -3,16 +3,16 @@
 int main()
 {   int b[25];
 int a[25];
-    for(int i=0;i<=25;i--){
+    for(int i=0;i<=25;i++){
         a[i]=0;
         b[i]=0;
     }
   
     b[1]=1;
     for (int j=1;j<=25;j++){
-        
-    a[j]=b[j-1]+b[j];
-    printf("%d\n",b[j-1]);
+    for (int k=1;k<=j;k++){
+    a[k]=b[k-1]+b[k];
+    }
     for(int i=1;i<=j;i++){
         
         printf("%d ",a[i]);
