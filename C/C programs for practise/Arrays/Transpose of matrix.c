ceb9e2463582bbd3ea584b30790dea7b160c940e
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-22 16:31:55 +0530

    Create Transpose of matrix.c

---
+++ C/C programs for practise/Arrays/Transpose of matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{   int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    
+     int c[3][3];
+     for(int i=0;i<3;i++){
+         for (int j=0;j<3;j++){
+             c[i][j]=a[j][i];
+             printf("%d ",c[i][j]);
+         }
+            printf("\n");
+     }
+     
+    printf("Hello World");
+
+    return 0;
+}
