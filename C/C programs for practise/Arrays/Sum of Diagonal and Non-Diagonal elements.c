df2954af826c39ae7dd160dae3abd9724f0d4c7a
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-22 16:44:17 +0530

    Create Sum of Diagonal and Non-Diagonal elements.c

---
+++ C/C programs for practise/Arrays/Sum of Diagonal and Non-Diagonal elements.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{   int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    
+     int c=0;
+     int d=0;
+     for(int i=0;i<3;i++){
+         for (int j=0;j<3;j++){
+             if(i==j)
+               c+=a[j][i];
+              else{
+                  d=d+a[j][i];
+              }
+            
+         }
+          
+     }
+     
+    printf("%d %d",c,d);
+
+    return 0;
+}
