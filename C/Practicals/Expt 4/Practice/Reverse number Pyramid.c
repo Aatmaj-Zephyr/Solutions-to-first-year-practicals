9f720f39b28007bae0e11e8b43d9a73b2e168aff
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-12-06 12:01:28 +0530

    Create Reverse number Pyramid.c

---
+++ C/Practicals/Expt 4/Practice/Reverse number Pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{   
+    for (int i=1;i<=6;i++){
+        
+        for(int j=1;j<=6-i;j++){
+            printf(" ");
+        }
+        for (int j=1;j<=i;j++){
+        printf("%d",j);
+        }
+        printf("\n");
+      }    
+    printf("Hello World");
+
+    return 0;
+}
