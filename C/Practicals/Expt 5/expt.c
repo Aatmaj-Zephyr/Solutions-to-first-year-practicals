f34867521203214ac556e1ef5701b721c7a5cf87
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-12-10 14:20:34 +0530

    Update expt.c

--- C/Practicals/Expt 5/expt.c
+++ C/Practicals/Expt 5/expt.c
@@ -1,9 +1,6 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
+Aatmaj Mhatre 16010121110 
+FY Comps B
 
 *******************************************************************************/
 #include <stdio.h>
