e01ac11cdaf39003bb1df4200daf296226718491
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-12-13 10:30:14 +0530

    Update PostLab.c

--- C/Practicals/Expt 5/PostLab.c
+++ C/Practicals/Expt 5/PostLab.c
@@ -1,22 +1,37 @@
-#include <stdio.h>
+/******************************************************************************
+Aatmaj Mhatre 16010121110 
+FY Comps B
 
+*******************************************************************************/
+#include <stdio.h>
+# define End -100000
+#define n 5
 int main()
-{  int a[5]={2,4,5,7,6};
-   int b[5];//making a new reverse std::array<
-   //alternative to this is to swap the first and the last elements.
-   
-   
-   for (int i=0;i<5;i++){
+{
+   signed int a[20];
+     signed int b[20];
+    signed int temp=0;
+    //initialize array with values from the user
+    for(int i=0;i<n;i++){//get first n values
+       
+       printf("... ");
+        scanf("%d",&temp);
+        a[i]=temp;
+    }
+    a[5]=End;
+    
+ for (int i=0;i<n;i++){
        b[4-i]=a[i];
        
    }
    
-   for (int i=0;i<5;i++){
+   for (int i=0;i<n;i++){
    printf("%d ",b[i]);
        
    }
   
-    printf("Hello World");
+   
+    
 
     return 0;
 }
