bd48b6bb4c24c619d616e4307c9a9d5a5c841e2e
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-12-17 14:33:22 +0530

    Added one more practical solution (#5)
    * Create main.yml
    
    * Delete .github/workflows directory
    
    * Create FILE_HEADER
    
    * Create main.yml
    
    * Delete .github/workflows directory
    
    * Create main.yml
    
    * Create expt+.c
    
    * Fixed error
    
    * Update main.yml
    
    * Create .header-config.json
    
    * Delete .github/workflows directory
    
    * Create main.yml
    
    * Delete .header-config.json
    
    * Delete .github/workflows directory
    
    * Delete FILE_HEADER

---
+++ C/Practicals/Expt 5/expt+.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main()
+{
+  signed int i,j,temp,k, a[20], size,size2, x;
+  for(int m=0;m<20;m++){
+      a[m]=-10000;
+  }
+
+  printf("Enter the size of the array: ");
+  scanf("%d",&size);
+  printf("Enter the Numbers: \n");
+  for(i=0;i<size;i++)
+    {
+      printf("Number %d: ",i+1);
+      scanf("%d",&a[i]);
+    }
+  printf("\nThe Numbers in Descending order: \n");
+  for(i=0;i<size;i++)
+    {
+      
+      for(j=0;j<size-1;j++)
+        {
+          if(a[j]<a[j+1])
+          {
+            temp = a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+          
+          }
+        }
+      
+      
+    }
+  for(i=0;i<size;i++)
+    {
+      printf("Number %d: %d\n",i+1,a[i]);
+    }
+  printf("\nEnter how many more numbers that you would like to add  ");
+  scanf("%d", &size2);
+  for(k=0;k<size2;k++)
+    {
+      printf("Number %d: ", k+1);
+      scanf("%d", &x);
+      for(i=0;i<size+k+1;i++)   
+        { 
+          if(x>a[i])
+            {
+              temp = a[i];
+              a[i] = x;
+              x = temp;
+            }
+        }
+    }
+  printf("\nThe Numbers in Decending order: \n");
+  for(i=0;i<size+size2;i++)
+    {
+      printf("Number %d: %d\n",i+1,a[i]);
+    }
+}
