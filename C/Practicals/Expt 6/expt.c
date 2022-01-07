2079f417e73848200c88b097bdd6eafe1147bda5
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-01-07 14:57:52 +0530

    fixed bugs, made functional

--- C/Practicals/Expt 6/expt.c
+++ C/Practicals/Expt 6/expt.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{   struct player{
+  struct player{
     
     int matches;
     int bestbowl;
@@ -9,55 +8,53 @@ int main()
 };
 struct player playerlist[10];//overflow error not considered.
 int choice=0;
-
-for(int i=0;i<=4;i++){//take 4 entries
-printf("Please enter player name  ");
-scanf("%s",playerlist[i].name);
-printf("Please enter player matches  ");
-scanf("%d",&playerlist[i].matches);
-printf("Please enter player best bowling figure  ");
-scanf("%d",&playerlist[i].bestbowl);
-printf("\n");
-}
 int length=4;//currrent length of the array
 char searcher[20];//for part d in searching the name
 int found=0; //for part d in finding the name
-
-printf("Please enter your choice\n");
-scanf("%d",&choice);
-
-
-switch(choice){
-case 1:
-////part a///////
-printf("Take an player \n");
-printf("Please enter player name  ");
-scanf("%s",playerlist[5].name);
-printf("Please enter player matches  ");
-scanf("%d",&playerlist[5].matches);
-printf("Please enter player best bowling figure  ");
-scanf("%d",&playerlist[5].bestbowl);
+void partd(){
+    
+   printf("Who do you want to search? ") ;
+ ///part d//////
+scanf("%s",searcher);
+ for(int i=0;i<=length;i++){
+     if(strcmp(searcher,playerlist[i].name)==0){
+printf("Found \n");
+printf("%s \n",playerlist[i].name);
+printf("%d \n",playerlist[i].matches);
+printf("%d \n",playerlist[i].bestbowl);
 printf("\n");
-length=length+1;
+found=1;
+break;
 
- //print the array
+}}
+if(found==0){
+    printf("Not found");
+
+} 
+}
+void partc(){
+     ////part c////////
+  //delete for second player.
+  printf("Deleting a element from the array \n");
+
+  for(int i=2;i<=length;i++){
+playerlist[i]=playerlist[i+1];
+       
+
+}
+length=length-1;
+//print the array
  for(int i=0;i<=length;i++){
 printf("%s \n",playerlist[i].name);
 printf("%d \n",playerlist[i].matches);
 printf("%d \n",playerlist[i].bestbowl);
 printf("\n");
 }
- 
- 
- 
- 
- 
- break;
- case 2:
- //////// part b/////
+}
+void partb(){//////// part b/////
  printf("Sorting the list \n");
  for(int j=0;j<=length;j++){
- for(int i=0;i<j;i++){
+ for(int i=0;i<length;i++){
 if(playerlist[i].matches>=playerlist[i+1].matches){
     if(playerlist[i].matches==playerlist[i+1].matches){
     if(playerlist[i].bestbowl>playerlist[i+1].bestbowl)
@@ -92,54 +89,67 @@ printf("%d             ",playerlist[i].matches);
 printf("%d \n",playerlist[i].bestbowl);
 printf("\n");
 }
-
-
- 
- break;
- case 3:
- ////part c////////
-  //delete for second player.
-  printf("Deleting a element from the array \n");
-
-  for(int i=2;i<=length;i++){
-playerlist[i]=playerlist[i+1];
-       
-
 }
-length=length-1;
-//print the array
+void parta(){
+    ////part a///////
+printf("Take an player \n");
+printf("Please enter player name  ");
+scanf("%s",playerlist[5].name);
+printf("Please enter player matches  ");
+scanf("%d",&playerlist[5].matches);
+printf("Please enter player best bowling figure  ");
+scanf("%d",&playerlist[5].bestbowl);
+printf("\n");
+length=length+1;
+
+ //print the array
  for(int i=0;i<=length;i++){
 printf("%s \n",playerlist[i].name);
 printf("%d \n",playerlist[i].matches);
 printf("%d \n",playerlist[i].bestbowl);
 printf("\n");
 }
+}
+int main()
+{ 
 
+for(int i=0;i<=4;i++){//take 4 entries
+printf("Please enter player name  ");
+scanf("%s",playerlist[i].name);
+printf("Please enter player matches  ");
+scanf("%d",&playerlist[i].matches);
+printf("Please enter player best bowling figure  ");
+scanf("%d",&playerlist[i].bestbowl);
+printf("\n");
+}
+
+
+printf("Please enter your choice\n");
+scanf("%d",&choice);
+
+
+switch(choice){
+case 1:
+parta();
+ 
+ break;
+ case 2:
+ partb();
+
+ 
+ break;
+ case 3:
+
+partc();
  
  
  
  break;
  case 4:
+ partd();
  
- printf("Who do you want to search? ") ;
- ///part d//////
-scanf("%s",searcher);
- for(int i=0;i<=length;i++){
-     if(strcmp(searcher,playerlist[i].name)==0){
-printf("Found \n");
-printf("%s \n",playerlist[i].name);
-printf("%d \n",playerlist[i].matches);
-printf("%d \n",playerlist[i].bestbowl);
-printf("\n");
-found=1;
-break;
-
-}}
-if(found==0){
-    printf("Not found");
-
-}
 break;//switch break
 }
     return 0;
 }
+
