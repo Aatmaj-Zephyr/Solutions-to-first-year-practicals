addf014293d850ca5b5d909f5395110a3e3c8f2d
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-01-28 15:18:07 +0530

    Update expt+.c

--- C/Practicals/Expt 5/expt+.c
+++ C/Practicals/Expt 5/expt+.c
@@ -1,3 +1,18 @@
+/****
+
+Please refrain from copying this material.
+
+#########
+This is for reference only. Please do not copy this material.
+This repository is ment only for promoting students and helping needy students, and not for encouraging any kind of malpratises or mass copying.
+Hence it is requested not to copy from this repository and use it only for reference when in doubt.
+######
+In case of any doubts please feel free to reach me via 
+- Gmail- aatmaj.mhatre@gmail.com
+- Github https://github.com/Aatmaj-Zephyr
+- Linkedin https://www.linkedin.com/in/aatmajmhatre/
+
+****/
 #include <stdio.h>
 
 int main()
