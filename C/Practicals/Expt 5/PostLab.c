66ecb6bf921a9fb5e6593d9a108a0f933cad971f
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-01-28 15:17:31 +0530

    Update PostLab.c

--- C/Practicals/Expt 5/PostLab.c
+++ C/Practicals/Expt 5/PostLab.c
@@ -1,8 +1,19 @@
-/******************************************************************************
-Aatmaj Mhatre 16010121110 
-FY Comps B
+/****
+Copyright @ 2021-2022 Aatmaj.
+Please refrain from copying this material.
 
-*******************************************************************************/
+#########
+This is for reference only. Please do not copy this material.
+This repository is ment only for promoting students and helping needy students, and not for encouraging any kind of malpratises or mass copying.
+Hence it is requested not to copy from this repository and use it only for reference when in doubt.
+######
+In case of any doubts please feel free to reach me via 
+- Gmail- aatmaj.mhatre@gmail.com
+- Github https://github.com/Aatmaj-Zephyr
+- Linkedin https://www.linkedin.com/in/aatmajmhatre/
+
+****/
+
 #include <stdio.h>
 # define End -100000
 #define n 5
