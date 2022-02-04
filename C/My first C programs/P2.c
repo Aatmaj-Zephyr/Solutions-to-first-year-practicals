09cf42b38ede0f1ad8b4311f09ce0902fd9ea1fa
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-02-04 09:48:25 +0530

    Create P2.c

---
+++ C/My first C programs/P2.c
@@ -0,0 +1,18 @@
+/* Write a C program to print a block C using hash (#), where the C has a height of
+six characters and width of five and four characters.*/
+
+#include <stdio.h>
+ int main()
+ {
+   printf("    ######\n");
+   printf("  ##      ##\n");
+   printf(" #\n");
+   printf(" #\n");
+   printf(" #\n");
+   printf(" #\n");
+   printf(" #\n");
+   printf("  ##      ##\n");
+   printf("    ######\n");
+
+	return(0);
+}
