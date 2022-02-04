9ba43194d765a382b9f66ff8c1663dfe6b5ef3ee
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-02-04 09:50:52 +0530

    Create P7.c

---
+++ C/My first C programs/P7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   double x = 5.5;
+   double y = 4.0;
+   double p;
+   p = pow(x, y);
+   printf("The value : %lf", p);
+   //printf("The value : %.2lf", p);
+   return 0;
+}
