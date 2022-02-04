2495c716142078153d387bae2301122c48f9c617
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-02-04 09:46:46 +0530

    Create P1.c

---
+++ C/My first C programs/P1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int main()
+  {
+     printf("Name   : Aatmaj-Zephyr\n");
+     printf("DOB    : 30 Feb 2020\n");
+     printf("Mobile : 99-9999999999\n");
+     return(0);
+  }
