ebd7a1226d04a28752985d486452a559f6acdf99
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-02-04 09:49:52 +0530

    Create P5.c

---
+++ C/My first C programs/P5.c
@@ -0,0 +1,17 @@
+// program for summation of two numbers
+
+
+#include <stdio.h>
+
+void main()
+   {
+	int x, y, sum;
+
+    printf("\nInput the first integer: ");
+    scanf("%d", &x);
+    printf("\nInput the second integer: ");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("\nSum of the above two integers = %d", sum);
+    //return 0;
+}
