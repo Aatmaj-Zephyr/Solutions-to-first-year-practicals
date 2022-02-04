1061f6fac5428b95d7808b7a05589dbd91db78a7
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-02-04 09:47:40 +0530

    Create P3.c

---
+++ C/My first C programs/P3.c
@@ -0,0 +1,16 @@
+/* Write a C program to print a block F using hash (#), where the F has a height of
+six characters and width of five and four characters.*/
+
+#include <stdio.h>
+ int main()
+ {
+	printf("######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+
+	return(0);
+}
