2e018cdb7d4f92a4cf729b10419bd6f038a37bb8
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-04-11 16:07:54 +0530

    Update prac1.py

--- python/Expt1/practise/prac1.py
+++ python/Expt1/practise/prac1.py
@@ -1,7 +1,10 @@
 name="Aatmaj"
 B="Zephyr"
 print(name.upper())
+print(len(name))
+print(name.replace("Aat","xyz"))
 print(name.lower())
 print(name[1:4])
 print(name[0:6:2])
-print(name+"-"+B)
+print(name+2*"\n")
+print(name+"\t"+B)
