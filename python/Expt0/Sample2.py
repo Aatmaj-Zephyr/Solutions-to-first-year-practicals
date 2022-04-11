2e538dbe08e760ed5ff1b18e604dfdb92cd674e6
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-04-11 15:17:36 +0530

    Create Sample2.py

---
+++ python/Expt0/Sample2.py
@@ -0,0 +1,19 @@
+my_name="Aatmaj" #my name
+admision_year=2021 #year of admission
+department="Computer science"# My department
+gender=True
+
+#print values
+print(my_name)
+print(admision_year)
+print(department)
+print(gender)
+
+#print types
+print(type(my_name))
+print(type(admision_year))
+print(type(department))
+print(type(gender))
+#multiple assignments
+a,b,c=1,2,"hi"
+print(a,b,c)
