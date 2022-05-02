bb72b9dec38418414641ea04ad65e432ac246391
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-02 04:42:01 -0700

    Update prob1.py

--- python/Expt4/prob1.py
+++ python/Expt4/prob1.py
@@ -2,3 +2,6 @@ import re
 a=r"Happy Birthday birthday! Have a good day. Ma0 a0b cbd Hope you are doing well. Get well soon."
 obj=re.findall("[\S]{1,}",a)
 print(obj)
+
+obj=re.search("[\S]{1,}",a)
+print(obj)
