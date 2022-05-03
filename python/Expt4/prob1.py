8b982e60729d30f1168f6ad725a5dee8f9b0ce01
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-03 12:55:41 +0530

    Update prob1.py

--- python/Expt4/prob1.py
+++ python/Expt4/prob1.py
@@ -1,7 +1,12 @@
 import re
-a=r"Happy Birthday birthday! Have a good day. Ma0 a0b cbd Hope you are doing well. Get well soon."
-obj=re.findall("[\S]{1,}",a)
-print(obj)
+a="Happy Birthday birthday! Have a good day. Ma0 a0b cbd Hope day you are doing well. Get well soon."
 
-obj=re.search("[\S]{1,}",a)
+obj=re.findall("[\w]{1,}",a)
+ctr=0 # position counter
 print(obj)
+for i in obj:
+    print(i,end="-")
+    obj2=re.search(i,a[ctr:]) #remove duplicate by reducing length of string
+    ctr=ctr+obj2.start()
+    print(ctr)
+
