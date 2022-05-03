eaedd361bd6cddff9b7b17e9f597ee20d800f1e1
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-03 12:29:02 +0530

    Update prob5.py

--- python/Expt4/prob5.py
+++ python/Expt4/prob5.py
@@ -1,6 +1,7 @@
 import re
 a=r"Happy Birthday birthday! Have a good day. dMa0gk a0bc ztgbhgy cbd Hope you are doing well. Get well soon."
 word="Happy" #word to be matched
-obj=re.findall("^Happy",a)
-#prints word if word matches else prints empty list.
+word="^"+word
+obj=re.findall(word,a)
+#prints word if wordber matches else prints empty list.
 print(obj)
