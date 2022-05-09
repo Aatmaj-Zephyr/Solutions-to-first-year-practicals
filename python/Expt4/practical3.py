afdc626c75a6dcc4d87297a96b149a2991b46d0d
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-05-09 10:54:08 +0000

    Automated autoyapf fixes

--- python/Expt4/practical3.py
+++ python/Expt4/practical3.py
@@ -1,4 +1,5 @@
 import re
+
 text = "Dave Martin 615-555-7164 173 Main St., Springfield RI 55924 davemartin@bogusemail.com Charles Harris 800-555-5669 969 High St., Atlantis VA 34075 charlesharris@bogusemail.com Eric Williams 560-555-5153 806 1st St., Faketown AK 86847 laurawilliams@bogusemail.com Corey Jefferson 900-555-9340 826 Elm St., Epicburg NE 10671 coreyjefferson@bogusemail.com"
 '''
 Expected output-
@@ -6,14 +7,15 @@ Expected output-
 
 
 '''
-numlist=[]
+numlist = []
 for txt in text.split():
-   # print(txt)
-    if(len(re.findall(r"\D", txt))==2): #check for letters
-     if(len(re.findall(r"-", txt))==2): #check for hyphens
-      if([len(i) for i in txt.split("-")]==[3,3,4]): #position of numbers
-       numlist.append(txt)
+    # print(txt)
+    if (len(re.findall(r"\D", txt)) == 2):  #check for letters
+        if (len(re.findall(r"-", txt)) == 2):  #check for hyphens
+            if ([len(i) for i in txt.split("-")] == [3, 3,
+                                                     4]):  #position of numbers
+                numlist.append(txt)
 print(numlist)
 
 #extrace email ids.
-print(re.findall(r"\S+@\S+.com",text))
+print(re.findall(r"\S+@\S+.com", text))
