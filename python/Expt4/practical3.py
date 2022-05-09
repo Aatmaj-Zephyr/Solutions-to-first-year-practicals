28afa5a3bd1d8c2712fff1566662bcc3d93db871
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-09 16:10:40 +0530

    Update practical3.py

--- python/Expt4/practical3.py
+++ python/Expt4/practical3.py
@@ -1,5 +1,4 @@
 import re
-
 txt = "Dave Martin 615-555-7164 173 Main St., Springfield RI 55924 davemartin@bogusemail.com Charles Harris 800-555-5669 969 High St., Atlantis VA 34075 charlesharris@bogusemail.com Eric Williams 560-555-5153 806 1st St., Faketown AK 86847 laurawilliams@bogusemail.com Corey Jefferson 900-555-9340 826 Elm St., Epicburg NE 10671 coreyjefferson@bogusemail.com"
 '''
 Expected output-
@@ -8,7 +7,9 @@ Expected output-
 
 '''
 for txt in txt.split():
-    # print(txt)
-    if (len(re.findall(r"\D", txt)) == 2):  #check for letters
-        if (len(re.findall(r"-", txt)) == 2):  #check for hyphens
-            print(txt)
+   # print(txt)
+    if(len(re.findall(r"\D", txt))==2): #check for letters
+     if(len(re.findall(r"-", txt))==2): #check for hyphens
+      if([len(i) for i in txt.split("-")]==[3,3,4]): #position of numbers
+       print(txt)
+     
