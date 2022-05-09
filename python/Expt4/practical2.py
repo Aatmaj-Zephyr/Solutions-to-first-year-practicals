6c0b65c72b6422fdcee9456f52f90e438b11a7ae
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-09 16:09:37 +0530

    Update practical2.py

--- python/Expt4/practical2.py
+++ python/Expt4/practical2.py
@@ -1,6 +1,6 @@
 import re
 
-txt = "423-3443-45590-3485"
+txt = "4213-3443-4590-3485"
 '''
 True test cases
 4523-3443-4590-3485
@@ -11,6 +11,7 @@ False test cases
 2345-4653-4567-7643
 453-234-566-33456
 4e56-3352-3456-3215
+2345-34-23456-12345
 
 
 '''
@@ -19,10 +20,10 @@ if (len(re.findall(r"\D", txt)) != 3):  #check for letters
 elif (len(re.findall(r"-", txt)) != 3):  #check for hyphens
     print("incorrect positioning of hyphens")
 
+
 elif (len(txt) != 16 + 3):  #check for word count
     print("incorrect word count")
-elif (["1" if (len(i) == 4) else "2" for i in txt.split("-")] !=
-      ['1', '1', '1', '1']):  #check for hyphens positions
+elif ([len(i) for i in txt.split("-")]!=[4, 4, 4, 4]):  #check for hyphens positions
     print("misplaced hyphens")
 elif (len(re.findall(r"^4|^5|^6", txt)) != 1):  #check for 456 beginning
     print("incorrect beginning")
