c908862a1f971df30f8d3f032d05d7f291249ade
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-09 15:46:37 +0530

    Create practical2.py

---
+++ python/Expt4/practical2.py
@@ -0,0 +1,25 @@
+import re
+txt = "4e56-3352-3456-3215"
+'''
+True test cases
+4523-3443-4590-3485
+5674-4444-5632-2456
+4354-3456-6788-9067
+
+False test cases
+2345-4653-4567-7643
+453-234-566-33456
+4e56-3352-3456-3215
+
+
+'''
+if(len(re.findall(r"\D", txt))!=3): #check for letters
+    print("incorrect - letters present")
+elif(len(re.findall(r"-", txt))!=3): #check for hyphens
+    print("incorrect positioning of hyphens")    
+elif(len(txt)!=16+3): #check for word count
+    print("incorrect word count") 
+elif(len(re.findall(r"^4|^5|^6", txt))!=1): #check for 456 beginning
+    print("incorrect beginning") 
+else:
+    print("correct")
