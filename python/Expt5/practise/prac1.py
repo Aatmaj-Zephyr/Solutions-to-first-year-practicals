c6da6c7735bac2e26aa3e73d784515fc268ed3cf
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-30 15:38:00 +0530

    Create prac1.py

---
+++ python/Expt5/practise/prac1.py
@@ -0,0 +1,22 @@
+'''
+a=[[1,2,3],[2,3,4],[3,4,5]]
+b=[[1,2,3],[2,3,4]]
+output
+Invalid matrix input
+'''
+'''
+c=[[1,2],[3,4]]
+d=[[5,6],[7,8]]
+output 
+[[19 22]
+ [43 50]]
+'''
+import numpy as np
+def mat_mul(p,q):
+    P=np.array(p)
+    Q=np.array(q)
+    try:
+     print(P@Q)
+    except:
+        print("Invalid matrix input")
+mat_mul(a,b)
