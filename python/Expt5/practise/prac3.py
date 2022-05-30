11d5bfd37728b8f6afdda7f3408532be52f8a8fb
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-30 16:30:09 +0530

    Update prac3.py

--- python/Expt5/practise/prac3.py
+++ python/Expt5/practise/prac3.py
@@ -1,27 +1,38 @@
-a = []
-for ptr in range(0, 10):
+a={}
+for ptr in range(0,10):
     try:
-        a.append(int(input("Please enter a value ")))
+        x=int(input("Please enter a value "))
     except:
-        print("Incorrect value")
+     print("Incorrect value")
+     continue
+    if x not in a:
+        a[x]=1
+    else:
+        a[x]=a[x]+1
+   
 print(a)
 print(max(a))
 print(min(a))
-b = []
-c = []
-for i in range(0, len(a)):
-    found = False
-    for j in range(0, len(b)):
-        if (a[i] == b[j][0]):
-            b[j][1] += 1
-            found = True
-    if (found == False):
-        b.append([a[i], 1])
-print(b)
-Max = 0
-Max2 = 0
-for i in b:
-    if (i[1] > Max2):
-        Max = i[0]
-        Max2 = i[1]
-print(Max)
+
+b=max(a.values())
+for i in a:
+    if a[i]==b:
+        print("Mode/s=",i)
+'''
+test cxase
+Please enter a value 1
+Please enter a value 2
+Please enter a value 3
+Please enter a value 4
+Please enter a value 5
+Please enter a value 1
+Please enter a value 2
+Please enter a value 1
+Please enter a value 1
+Please enter a value 78
+{1: 4, 2: 2, 3: 1, 4: 1, 5: 1, 78: 1}
+78
+1
+Mode/s= 1
+
+'''
