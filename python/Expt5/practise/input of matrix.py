dda5b81691d686f1f27aa903f16504152a44cbdf
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-05-30 11:25:36 +0000

    Automated autoyapf fixes

--- python/Expt5/practise/input of matrix.py
+++ python/Expt5/practise/input of matrix.py
@@ -1,8 +1,8 @@
 #take matrix input
 def mat_input():
-    m=int(input("Please enter m "))
-    n=int(input("Please enter n "))
-    a=[]
+    m = int(input("Please enter m "))
+    n = int(input("Please enter n "))
+    a = []
     for i in range(m):
         a.append([])
         for j in range(n):
