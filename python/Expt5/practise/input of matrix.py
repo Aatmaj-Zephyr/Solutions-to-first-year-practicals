94a57e34546df22f412c7f7f047369de15afd0bb
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-30 16:52:12 +0530

    Create input of matrix.py

---
+++ python/Expt5/practise/input of matrix.py
@@ -0,0 +1,11 @@
+#take matrix input
+def mat_input()
+    m=int(input("Please enter m "))
+    n=int(input("Please enter n "))
+    a=[]
+    for i in range(m):
+        a.append([])
+        for j in range(n):
+            a[i].append(input("."))
+    print(a)
+    return a
