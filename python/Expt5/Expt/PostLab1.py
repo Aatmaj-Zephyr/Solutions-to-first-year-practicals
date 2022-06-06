daa989e764e839b1f670d4e5227d1f5237bff34b
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-06-06 14:27:22 +0000

    Automated autoyapf fixes

--- python/Expt5/Expt/PostLab1.py
+++ python/Expt5/Expt/PostLab1.py
@@ -1,9 +1,10 @@
 def factorial(x):
-    if(x<0):
-        return 0 #error
-    if(x-1!=0):
-     return x*factorial(x-1)
+    if (x < 0):
+        return 0  #error
+    if (x - 1 != 0):
+        return x * factorial(x - 1)
     else:
         return 1
+
 
 print(factorial(4))
