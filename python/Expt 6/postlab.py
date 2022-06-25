6e28250ce88d5b3a6efa6969ec6ae68dd3eb4c32
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-06-25 11:24:37 +0000

    Automated autoyapf fixes

--- python/Expt 6/postlab.py
+++ python/Expt 6/postlab.py
@@ -1,30 +1,31 @@
 class store():
-    products={'book':70,'pen':10,'gelpen':15,'pencil':5}
+    products = {'book': 70, 'pen': 10, 'gelpen': 15, 'pencil': 5}
     #dictionary to store the product and prices
-    cart= dict.fromkeys(products, 0) # make dictioanry with zero values
+    cart = dict.fromkeys(products, 0)  # make dictioanry with zero values
 
     def display(self):
         for key in self.products:
-            print(key,end="\t\t")
+            print(key, end="\t\t")
             print(self.products[key])
+
     def prompt(self):
         for key in self.cart:
-            print(key,end="-")
-            self.cart[key]=input("Enter amount for the product ")
+            print(key, end="-")
+            self.cart[key] = input("Enter amount for the product ")
 
     def bill(self):
-        net=0
+        net = 0
         for key in self.products:
-          print(key,end="\t\t")
-          print(self.cart[key],end="\t")
-          print(self.products[key],end="\t\t")
-          print(int(self.cart[key])*int(self.products[key])) #error handling not done for integer casting
-          net+=int(self.cart[key])*int(self.products[key])
+            print(key, end="\t\t")
+            print(self.cart[key], end="\t")
+            print(self.products[key], end="\t\t")
+            print(int(self.cart[key]) * int(self.products[key])
+                  )  #error handling not done for integer casting
+            net += int(self.cart[key]) * int(self.products[key])
         print(net)
 
 
-
-mystore=store()
+mystore = store()
 mystore.display()
 mystore.prompt()
 mystore.bill()
