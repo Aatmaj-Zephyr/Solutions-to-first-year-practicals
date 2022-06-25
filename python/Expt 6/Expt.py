ab7b16d21ebc56ce600f59e38058eb45fa8e0583
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-06-25 11:23:55 +0000

    Automated autoyapf fixes

--- python/Expt 6/Expt.py
+++ python/Expt 6/Expt.py
@@ -1,39 +1,42 @@
 class student:
-    def __init__(self,name,Rollno):
-        self.name=name
-        self.Rollno=Rollno
-        
-    def setPhysicsMarks(self,marks):
-        self.physicsmarks=marks
-        
-    def setMathsMarks(self,marks):
-        self.Mathsmarks=marks
-        
-    def setEEMarks(self,marks):
-        self.EEmarks = marks 
-        
-    def setMechMarks(self,marks):
+
+    def __init__(self, name, Rollno):
+        self.name = name
+        self.Rollno = Rollno
+
+    def setPhysicsMarks(self, marks):
+        self.physicsmarks = marks
+
+    def setMathsMarks(self, marks):
+        self.Mathsmarks = marks
+
+    def setEEMarks(self, marks):
+        self.EEmarks = marks
+
+    def setMechMarks(self, marks):
         self.MechMarks = marks
-    
+
     def getPhysicsMarks(self):
         return self.physicsmarks
-        
+
     def getMathsMarks(self):
         return self.Mathsmarks
-        
+
     def getEEMarks(self):
         return self.EEmarks
-        
+
     def getMechMarks(self):
         return self.MechMarks
-    
+
     def total(self):
-        return sum([self.physicsmarks,self.MechMarks,self.Mathsmarks,self.EEmarks])
-        
+        return sum(
+            [self.physicsmarks, self.MechMarks, self.Mathsmarks, self.EEmarks])
+
     def percentge(self):
-        return self.total()/4*100
-        
-Aditya=student("Aditya",121)
+        return self.total() / 4 * 100
+
+
+Aditya = student("Aditya", 121)
 Aditya.setMechMarks(18)
 Aditya.setEEMarks(10)
 Aditya.setMathsMarks(25)
