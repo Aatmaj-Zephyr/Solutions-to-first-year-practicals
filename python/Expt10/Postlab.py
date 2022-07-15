e7ddedb9712b2fa6ca1f3d3c025060fc6ca60103
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-07-15 20:12:58 +0530

    Create Postlab.py

---
+++ python/Expt10/Postlab.py
@@ -0,0 +1,6 @@
+try:
+    file=open(input("Please enter a valid file name "),"r")
+    for i in file:
+        print(i.upper())
+except FileNotFoundError:
+    print("please enter  valid file name ")
