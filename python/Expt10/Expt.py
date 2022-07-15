6a836a4cbc2963be5b1a92eff4c0dac1900a49af
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-07-15 20:12:16 +0530

    Create Expt.py

---
+++ python/Expt10/Expt.py
@@ -0,0 +1,1 @@
+[open("rollcall.txt","a").write("\t\t\t".join([input("Please enter Roll number "),input("Please enter name "),input("Please enter department "),"\n"])) for i in range(0,3)] # one line solution
